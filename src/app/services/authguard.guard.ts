f50069fa8aaedf400b965d95b3393abc8580825c
captainspeak <captainspeak@gmail.com>
2025-05-31 22:09:09 +0000

    updating package.json from 15 to Angular 16

--- src/app/services/authguard.guard.ts
+++ src/app/services/authguard.guard.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  CanActivate,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserDetailService } from './user-detail.service';
 import { NavController } from '@ionic/angular';
@@ -11,7 +7,7 @@ import { NavController } from '@ionic/angular';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthguardGuard implements CanActivate {
+export class AuthguardGuard  {
   constructor(
     private _userDetailService: UserDetailService,
     private _navController: NavController,
