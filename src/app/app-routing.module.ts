dfc523bf6489b51ff40d4fdaeeb3cc9671b6080c
captainspeak <captainspeak@gmail.com>
2025-05-31 22:04:56 +0000

    updated package.json and Angular.json to Angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,7 @@ const routes: Routes = [
 @NgModule({
 	imports: [
 		RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules,
-    relativeLinkResolution: 'legacy'
+    preloadingStrategy: PreloadAllModules
 }),
 	],
 	exports: [RouterModule],
